fdc030692a60d23132d55eb0cbc16af96fe44e20
Darkb0shy <dcarassi@localhost.localdomain>
2023-10-19 10:14:51 +0200

    bombardatemi

--- srcs/check_background_color.c
+++ srcs/check_background_color.c
@@ -186,12 +186,12 @@ void    check_background_color(t_game *game)
 {
     int i;
     int j;
+    int flag;
 
+    flag = 0;
     i = 0;
     while (game->cub_file.file_matrix[i])
     {
-        // if (game->cub_file.f_color && game->cub_file.c_color)
-        //     break ;
         j = 0;
         while (game->cub_file.file_matrix[i][j] && game->cub_file.file_matrix[i][j] == 32)
             j++;
@@ -212,18 +212,24 @@ void    check_background_color(t_game *game)
         }
         else if (game->cub_file.file_matrix[i][j] == 'C')
         {
-            if (game->cub_file.c_color)
+            if (!game->cub_file.f_color)
             {
+                free(game->cub_file.c_color);
+                std_errore("F RGB value must come before C value\n");
+            }
+            if (flag == 1)
+            {
+                free(game->cub_file.f_color);
                 free(game->cub_file.c_color);
                 std_errore("double C identifier\n");
             }
             else
             {
                 game->cub_file.c_color = ft_strdup(game->cub_file.file_matrix[i]);
-                break ;
+                flag = 1;
             }
         }
-        if (ft_isdigit(game->cub_file.file_matrix[i][j]))
+        if ((!game->cub_file.c_color && !game->cub_file.f_color) && ft_isdigit(game->cub_file.file_matrix[i][j]))
             std_errore("map must be last element of the .cub file!\n");
         i++;
     }
--- srcs/init_player_and_textures.c
+++ srcs/init_player_and_textures.c
@@ -102,11 +102,6 @@ void init_player_and_textures(t_game *game)
 {
     init_player(game);
     game->mlx = mlx_init();
-    game->win = mlx_new_window(game->mlx, 1280, 720, "cub3d");
-    game->screen.img = mlx_new_image(game->mlx, 1280, 720);
-	game->screen.addr = mlx_get_data_addr(game->screen.img,
-			&game->screen.bits_per_pixel, &game->screen.line_length,
-			&game->screen.endian);
 	game->walls[0].img = mlx_xpm_file_to_image(game->mlx,
 			game->map->nord, &(game->wall_heights[0]), &(game->wall_widths[0]));
 	if (game->walls[0].img == NULL)
@@ -135,4 +130,9 @@ void init_player_and_textures(t_game *game)
 	game->walls[3].addr = mlx_get_data_addr(game->walls[3].img,
 			&game->walls[3].bits_per_pixel, &game->walls[3].line_length,
 			&game->walls[3].endian);
+	game->win = mlx_new_window(game->mlx, 1280, 720, "cub3d");
+    game->screen.img = mlx_new_image(game->mlx, 1280, 720);
+	game->screen.addr = mlx_get_data_addr(game->screen.img,
+			&game->screen.bits_per_pixel, &game->screen.line_length,
+			&game->screen.endian);
 }
